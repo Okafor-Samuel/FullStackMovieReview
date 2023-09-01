95e32567da80426bc517e129df306c16749acd02
decagon <samuel.okafor@decagon.dev>
2023-09-01 14:40:16 +0100

    update

--- src/main/java/com/example/movieapi/Controller/MovieController.java
+++ src/main/java/com/example/movieapi/Controller/MovieController.java
@@ -1,16 +1,32 @@
 package com.example.movieapi.Controller;
 
+import com.example.movieapi.Entity.Movie;
+import com.example.movieapi.Service.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
+@RequiredArgsConstructor
 public class MovieController {
+    private final MovieService movieService;
     @GetMapping
-    public ResponseEntity<String> allMovies(){
-        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies(){
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(),HttpStatus.OK);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getAMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<Movie>(movieService.singleMovie(id),HttpStatus.OK);
+
+
     }
 }
+
