1ebed1b51ad6851cd914fcd8321815aa916d11fb
decagon <samuel.okafor@decagon.dev>
2023-09-01 14:39:28 +0100

    update

---
+++ src/main/java/com/example/movieapi/Exception/CustomExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.example.movieapi.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handleException(MethodArgumentNotValidException ex){
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error->errors.put(error.getField(),error.getDefaultMessage()));
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(MovieNotFoundException.class)
+    public Map<String, String> movieNotFound(MovieNotFoundException ex){
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        return error;
+    }
+}
