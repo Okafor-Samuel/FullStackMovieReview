97f215c0fbca008dbb6385235ec4532e69d94ec9
decagon <samuel.okafor@decagon.dev>
2023-09-01 13:56:15 +0100

    update

--- src/main/java/com/example/movieapi/MovieApiApplication.java
+++ src/main/java/com/example/movieapi/MovieApiApplication.java
@@ -1,7 +1,12 @@
 package com.example.movieapi;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @SpringBootApplication
 public class MovieApiApplication {
@@ -10,4 +15,12 @@ public class MovieApiApplication {
         SpringApplication.run(MovieApiApplication.class, args);
     }
 
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Document(collection = "reviews")
+    public static class Review {
+        private ObjectId id;
+        private String reviewBody;
+    }
 }
