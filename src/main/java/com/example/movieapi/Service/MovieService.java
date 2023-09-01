d6144cf4da57fc89638f55044d27c6fb599b1ff7
decagon <samuel.okafor@decagon.dev>
2023-09-01 14:40:40 +0100

    update

--- src/main/java/com/example/movieapi/Service/MovieService.java
+++ src/main/java/com/example/movieapi/Service/MovieService.java
@@ -1,7 +1,22 @@
 package com.example.movieapi.Service;
 
+import com.example.movieapi.Entity.Movie;
+import com.example.movieapi.Exception.MovieNotFoundException;
+import com.example.movieapi.Repository.MovieRepository;
+import lombok.RequiredArgsConstructor;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class MovieService {
+    private final MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll();
+    }
+    public Movie singleMovie(ObjectId id){
+        return movieRepository.findById(id).orElseThrow(()-> new MovieNotFoundException("Movie with id "+id+" not found"));
+    }
 }
