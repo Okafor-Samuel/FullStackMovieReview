a08fd5c643dcc2e84b618f6ccd88db8f079bfd7c
decagon <samuel.okafor@decagon.dev>
2023-09-01 15:09:48 +0100

    update

--- src/main/java/com/example/movieapi/Entity/Review.java
+++ src/main/java/com/example/movieapi/Entity/Review.java
@@ -4,12 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "reviews")
 public class Review {
+        @Id
         private ObjectId id;
         private String reviewBody;
+
+        public Review(String reviewBody) {
+                this.reviewBody = reviewBody;
+        }
 }
