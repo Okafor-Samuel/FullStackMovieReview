36d29c8d410f5e6564e3dd385962fde783afdbf4
decagon <samuel.okafor@decagon.dev>
2023-09-01 13:52:22 +0100

    update

---
+++ src/main/java/com/example/movieapi/Controller/MovieController.java
@@ -0,0 +1,16 @@
+package com.example.movieapi.Controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    public ResponseEntity<String> allMovies(){
+        return new ResponseEntity<String>("All Movies", HttpStatus.OK);
+    }
+}
