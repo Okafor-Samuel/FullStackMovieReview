2fb03191fb309be93155f39b03a5cc77b9a09f0d
decagon <samuel.okafor@decagon.dev>
2023-09-01 15:10:02 +0100

    update

--- src/main/java/com/example/movieapi/Service/ReviewService.java
+++ src/main/java/com/example/movieapi/Service/ReviewService.java
@@ -1,12 +1,28 @@
 package com.example.movieapi.Service;
 
+import com.example.movieapi.Entity.Movie;
+import com.example.movieapi.Entity.Review;
 import com.example.movieapi.Repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ReviewService {
     private final ReviewRepository reviewRepository;
+    private final MongoTemplate mongoTemplate;
 
+    public Review createReview(String reviewBody, String imdbId){
+        var review = reviewRepository.insert(new Review(reviewBody));
+
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+        return review;
+
+    }
 }
