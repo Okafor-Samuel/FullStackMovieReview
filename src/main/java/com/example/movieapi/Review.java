989787f285988e9a88f1f01101fa10056f501437
decagon <samuel.okafor@decagon.dev>
2023-09-01 13:47:01 +0100

    update

---
+++ src/main/java/com/example/movieapi/Review.java
@@ -0,0 +1,16 @@
+package com.example.movieapi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "reviews")
+public class Review {
+    private ObjectId id;
+    private String reviewBody;
+}
